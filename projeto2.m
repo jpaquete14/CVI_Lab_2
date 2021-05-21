2df86d43e535d992bf3a7d3c7afb34c5dcdf6fe0
Maciej St <maciekstosio@icloud.com>
2021-05-21 23:21:04 +0100

    Add changes

--- projeto2.m
+++ projeto2.m
@@ -1,3 +1,5 @@
+clear all, close all
+
 frames = dir('Crowd_PETS09/S2/L1/Time_12-34/View_001/*.jpg');      
 nFrames = length(frames);
 %figure,
@@ -8,7 +10,7 @@ frame = imread(frameName);
 [rows, columns, numberOfColorChannels] = size(frame);
 
 vid4D = zeros([rows columns 3 nFrames/step]);
-
+%% Background estimation
 k = 1;
 for i=1:step:nFrames
     frameName = ['Crowd_PETS09/S2/L1/Time_12-34/View_001/' frames(i).name];
@@ -20,17 +22,19 @@ for i=1:step:nFrames
 end
 bkg = median(vid4D,4);
 figure('Name', 'Background'),imshow(uint8(bkg));
-
+%% Diff between two images 
 figure
 frame1 = imread(['Crowd_PETS09/S2/L1/Time_12-34/View_001/' frames(1).name]);
 imshow(im2bw(frame1));
 figure
-frame2 = imread(['Crowd_PETS09/S2/L1/Time_12-34/View_001/' frames(5).name]);
+frame2 = imread(['Crowd_PETS09/S2/L1/Time_12-34/View_001/' frames(2).name]);
 imshow(im2bw(frame2));
 figure
-diff = imsubtract(uint8(im2bw(frame1)), uint8(im2bw(frame2)));
+diff = imclose(abs(imsubtract(im2bw(frame2), im2bw(frame1))), strel('disk',1));
 imshow(diff);
-%%
+opflow = opticalFlow(im2double(im2bw(frame2)),im2double(im2bw(frame1)));
+plot(opflow,'DecimationFactor',[10 10],'ScaleFactor',10);
+%% Bounding boxes
 for f=1:nFrames
     f
 %     figure('Name', 'Image Subtraction');
@@ -54,4 +58,38 @@ for f=1:nFrames
     end
     pause(0.2)
 end
+%% Simple algorithm 
+close all
 
+figure
+for f=2:nFrames
+    frame1 = imread(['Crowd_PETS09/S2/L1/Time_12-34/View_001/' frames(f-1).name]);
+    frame2 = imread(['Crowd_PETS09/S2/L1/Time_12-34/View_001/' frames(f).name]);
+    diff = imclose(abs(imsubtract(im2bw(frame2), im2bw(frame1))), strel('disk',1));
+    [lb num]=bwlabel(diff);
+    regionProps = regionprops(lb,'centroid', 'area', 'perimeter', 'BoundingBox');
+
+    imshow(frame1);
+    for i=1:num
+        if regionProps(i).Area > 200
+            rectangle('Position',[regionProps(i).BoundingBox(1),regionProps(i).BoundingBox(2),regionProps(i).BoundingBox(3),regionProps(i).BoundingBox(4)],...
+            'EdgeColor','r','LineWidth',2 )
+        end    
+    end
+    pause(0.2)
+end
+
+%% Motion field
+close all
+
+figure
+for f=2:nFrames
+    frame1 = imread(['Crowd_PETS09/S2/L1/Time_12-34/View_001/' frames(f-1).name]);
+    frame2 = imread(['Crowd_PETS09/S2/L1/Time_12-34/View_001/' frames(f).name]);
+
+    hold on;
+    opflow = opticalFlow(im2double(im2bw(frame2)),im2double(im2bw(frame1)));
+    plot(opflow,'DecimationFactor',[10 10],'ScaleFactor',10);
+    hold off;
+    pause(0.2)
+end
